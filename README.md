492053b290ee5b90a623aaf24debc78108b380bf
JazZ <aleber@team-ever.com>
2017-07-22 11:05:27 +0400

    Add script title

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Ever Domain Checker v0.1
 
-EverDomainChecker is a PHP script using cURL to check the domain status for a
-given list. 
+EverDomainChecker is a PHP script using cURL to check the http status for a
+given domain list. 
 
 The script uses (and requires) the RollingCurlX PHP classe shared by [marcushat](https://github.com/marcushat).
 You can find the repository here : https://github.com/marcushat/RollingCurlX
--- ever_domain_checker.php
+++ ever_domain_checker.php
@@ -1,22 +1,34 @@
 <?php
 
-/**
- * Ever Domain Checker 
- */
+echo "
+ --------------------------------------------------------------------------------
+|   _____   _____      _      __  __           _____  __     __  _____   ____    |
+|  |_   _| | ____|    / \    |  \/  |         | ____| \ \   / / | ____| |  _ \   |
+|    | |   |  _|     / _ \   | |\/| |  _____  |  _|    \ \ / /  |  _|   | |_) |  |
+|    | |   | |___   / ___ \  | |  | | |_____| | |___    \ V /   | |___  |  _ <   |
+|    |_|   |_____| /_/   \_\ |_|  |_|         |_____|    \_/    |_____| |_| \_\  |
+|                               Domain Checker v0.1                              |
+ --------------------------------------------------------------------------------
+";
+sleep(3);
 
 // require curl multi-requests handler
 require('../RollingCurlX/src/rollingcurlx.class.php');
 
 // require domain list
 $urls = require('domain_list.php');
+$urls_count = count($urls);
+
 
 /**
  * Callback function
  */
 function everDomainChecker($response, $url, $request_info, $user_data, $time) 
 {
-    echo "The domain {$request_info['url']} is {$request_info['curle_msg']} with a status code of {$request_info['http_code']}: \n------------------\n";
+    echo "The domain {$request_info['url']} is {$request_info['curle_msg']} with a status code of {$request_info['http_code']}";
+    echo "\n-----------------------------------------\n";
 }
+
 
 // rollingCurlX init
 $RCX = new RollingCurlX(20);
@@ -34,3 +46,6 @@ foreach ($urls as $url) {
 }
 
 $RCX->execute();
+
+// some stats
+echo "{$urls_count} URLs have been checked";
